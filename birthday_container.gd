3093f6fb6befa21d719e61c6f455e447e3d37b25
Bucket Of Chicken <122376377+notdraimdev@users.noreply.github.com>
2025-06-29 10:29:52 +0200

    simplaudio birthday

--- birthday_container.gd
+++ birthday_container.gd
@@ -3,6 +3,8 @@ extends Control
 @onready var birthday_particles: GPUParticles2D = $BirthdayParticles
 @onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
 @onready var birthday_happy: Label = $"../BirthdayHappy"
+@export var opacitycurve:Curve
+var opacity:float = 1
 
 var awaitingJumpscare:bool
 var IsFocused:bool
@@ -11,12 +13,11 @@ func _ready() -> void:
 	birthday_happy.hide()
 	print("user: ",DiscordRPC.get_current_user().get("username"))
 	var bday:bool
-	if owner.DiscordUsername == "woostudiosjohn":
-		if Time.get_datetime_dict_from_system().day == 5:
-			if Time.get_datetime_dict_from_system().month == 11:
-				await get_tree().create_timer(0.2).timeout
-				awaitingJumpscare = true
-				bday = true
+	if Time.get_datetime_dict_from_system().day == 29:
+		if Time.get_datetime_dict_from_system().month == 6:
+			await get_tree().create_timer(0.2).timeout
+			awaitingJumpscare = true
+			bday = true
 
 func _notification(what):
 	if what == get_tree().NOTIFICATION_APPLICATION_FOCUS_IN:
@@ -27,9 +28,10 @@ func _notification(what):
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if awaitingJumpscare && IsFocused:
-			birthday_happy.show()
-			awaitingJumpscare = false
-			birthday_particles.emitting = true
-			audio_stream_player.play()
+		birthday_happy.show()
+		awaitingJumpscare = false
+		birthday_particles.emitting = true
+		audio_stream_player.play()
 	if !awaitingJumpscare:
-		birthday_happy.modulate.a -= delta/5
+		opacity -= delta/5
+		birthday_happy.modulate.a = opacitycurve.sample_baked(opacity)
