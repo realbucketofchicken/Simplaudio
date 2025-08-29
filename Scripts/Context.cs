89ba4790da97057f8ca0eaf36d7dd66586656315
Bucket Of Chicken <122376377+realbucketofchicken@users.noreply.github.com>
2025-08-29 19:40:44 +0200

    functional directory picker

--- Scripts/Context.cs
+++ Scripts/Context.cs
@@ -3,5 +3,14 @@ using System;
 
 public partial class Context : Node
 {
+	
 	String[] Songs;
+	DirectoryManager manager = new();
+
+	 
+
+	public void LoadDirectory(String path){
+		GD.Print(path);
+		manager.LoadDirectory(path);
+	}
 }
--- Scripts/Data.cs
+++ Scripts/Data.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.IO;
 using System.Linq;
 using Godot;
+using TagLib;
 using TagLib.Riff;
 
 
@@ -11,6 +12,7 @@ public class Song{
 	public String[] Artists;
 	public String Album;
 	public float Length;
+	public String Comment;
 	public String Directory;
 }
 public class DirectoryManager{
@@ -21,20 +23,27 @@ public class DirectoryManager{
 			if (!(file.EndsWith(".mp3") || file.EndsWith(".ogg") || file.EndsWith(".wav"))){
 				continue;
 			}
-			var tfile = TagLib.File.Create(file);
+			var TLfile = TagLib.File.Create(file);
 
 			Song song = new();
-			song.Album = tfile.Tag.Album;
-			song.Artists = tfile.Tag.Performers;
-			song.Name = tfile.Tag.Title;
-			song.Length = tfile.Length;
+			song.Album = TLfile.Tag.Album;
+			song.Artists = TLfile.Tag.Performers;
+			song.Name = TLfile.Tag.Title;
+			song.Length = TLfile.Length;
 			song.Directory = file;
-			GD.Print("File Valid, file path ",tfile.Tag.Title);
+			song.Comment = TLfile.Tag.Comment;
+			if (file.EndsWith(".mp3")){
+				song.Comment = TLfile.GetTag(TagLib.TagTypes.Id3v2, true).Comment;
+			}
+			//TLfile.Tag.CopyTo(Tag)
+			
+
+			GD.Print("File Valid, file path ",song.Directory);
 			Songs.Append(song);
 
 			//if (file.EndsWith(".mp3"))
 		}
 
-		return [];
+		return Songs;
 	}
 }
