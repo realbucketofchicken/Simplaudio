30282d5ba383139b9b36312eea672f7b2b7ea18e
Bucket Of Chicken <122376377+realbucketofchicken@users.noreply.github.com>
2025-09-28 15:17:38 +0200

    translations and more UI  work

---
+++ Scripts/SourceDisplay.cs
@@ -0,0 +1,9 @@
+using Godot;
+using System;
+
+public partial class SourceDisplay : Control
+{
+    [Export] public Label NameLabel;
+    [Export] public Label PathLabel;
+    [Export] public CheckButton CheckButtonButton;
+}
